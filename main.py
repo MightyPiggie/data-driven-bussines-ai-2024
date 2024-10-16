8eaaf59e6a53ea85ae1c25cde87253bb24016b7c
Daan Smienk <daansmienk10@gmail.com>
2024-10-16 11:34:17 +0200

    Update main

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import folium
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtWebEngineWidgets import QWebEngineView
+import random
+import webbrowser
+
+
 if __name__ == '__main__':
-    print('Hello, World!')
+    app = QtWidgets.QApplication([])
+    view = QWebEngineView()
+    m = folium.Map(location=(52.0838811 , 5.1756689))
+    html = m.get_root().render()
+    view.setHtml(html)
+    view.show()
+    app.exec()
+    print("test")
+    
 
         
