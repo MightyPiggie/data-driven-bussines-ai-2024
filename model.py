bf3da21b99b1f98fe7dd268aa7e3528758b28a1f
Daan Smienk <daansmienk10@gmail.com>
2024-10-22 11:25:16 +0200

    added new bins, added model training and score and some more

--- model.py
+++ model.py
@@ -2,24 +2,29 @@ from sklearn.ensemble import BaggingClassifier # https://scikit-learn.org/stable
 from sklearn.ensemble import RandomForestClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.ExtraTreesClassifier.html#sklearn.ensemble.ExtraTreesClassifier
 from sklearn.ensemble import VotingClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.VotingClassifier.html#sklearn.ensemble.VotingClassifier
-from sklearn.mixture import BayesianGaussianMixture #https://scikit-learn.org/stable/modules/generated/sklearn.mixture.BayesianGaussianMixture.html
 from sklearn.naive_bayes import GaussianNB #https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB
-from sklearn.mixture import GaussianMixture #https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html#sklearn.mixture.GaussianMixture
-from sklearn.neighbors import NearestNeighbors #https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier #https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
+from lineartree import LinearTreeClassifier, LinearForestClassifier, LinearBoostClassifier #https://github.com/cerlymarco/linear-tree
+from sklearn.linear_model import RidgeClassifier, Ridge
+from sklearn.multiclass import OneVsRestClassifier
+import warnings
+warnings.simplefilter('ignore')
 
 models = {
     "BaggingClassifier": BaggingClassifier(),
     "RandomForestClassifier": RandomForestClassifier(),
     "ExtraTreesClassifier": ExtraTreesClassifier(),
-    "BayesianGaussianMixture": BayesianGaussianMixture(),
     "GaussianNB": GaussianNB(),
-    "GaussianMixture": GaussianMixture(),
-    "NearestNeighbors": NearestNeighbors(),
-    "MLPClassifier": MLPClassifier()
+    "NearestNeighborsClassifier": KNeighborsClassifier(),
+    "MLPClassifier": MLPClassifier(),
+    "LinearTreeClassifier": LinearTreeClassifier(base_estimator=RidgeClassifier()),
+    "LinearForestClassifier": OneVsRestClassifier(LinearForestClassifier(Ridge(), max_features="sqrt")),
+    "LinearBoostClassifier": LinearBoostClassifier(base_estimator=RidgeClassifier()),
 }
 
-def train_models(models, X_train, y_train, X_test, y_test):
+def train_models(models, X_train, X_test, y_train, y_test):
+    print("Training models...")
     for name, model in models.items():
         model.fit(X_train, y_train)
         score = model.score(X_test, y_test)
